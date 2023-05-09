01de87579aa322f065b3921ee34340b671532d80
AW <awlodawiec@st.swps.edu.pl>
2023-05-09 14:51:42 +0200

    added modal register

--- script.js
+++ script.js
@@ -9,9 +9,11 @@ function openTab(tabName) {
 
 var modalPrzelew = document.getElementById('Przelew');
 var modalKredyt = document.getElementById('Kredyt');
+var modalRegister = document.getElementById('Register');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modalKredyt) {modalKredyt.style.display = "none";}
     if (event.target == modalPrzelew) { modalPrzelew.style.display = "none";}
+    if (event.target == modalRegister) { modalRegister.style.display = "none";}
 }

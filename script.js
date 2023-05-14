33aecf2e3fac036c99a0ef62f38e4182832ffc1c
AW2077 <aga.wlodawiec@gmail.com>
2023-05-14 22:29:31 +0200

    cleaning

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-function openTab(tabName) {
-    var i, x;
-    x = document.getElementsByClassName("tab");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-    }
-    document.getElementById(tabName).style.display = "block";
-  } //na razie nic nie używa tej funkcji, ale może będzie, więc na razie nie usuwam
-
 var modalPrzelew = document.getElementById('Przelew');
 var modalKredyt = document.getElementById('Kredyt');
 var modalRegister = document.getElementById('Register');
